7517a48eb098e650efc0d9c39bfe9e6437ea2938
Ekaterina Murzina <kate.m1992@yandex.ru>
2025-01-11 15:39:26 +0300

    Внесены изменения в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(cardContent) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = cardContent.link;
+    cardImage.alt = cardContent.name;
+    cardTitle.textContent = cardContent.name;
+    deleteButton.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+function showCards(cards) {
+    cards.forEach(cardContent => {
+        const card = createCard(cardContent);
+        placesList.append(card);
+    });
+}
+showCards(initialCards);
